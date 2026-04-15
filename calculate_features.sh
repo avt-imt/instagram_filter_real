f23fb7694b3cfa2ebff7e3c53a1508ff88347688
Steve Göring <steve.goering@tu-ilmenau.de>
2026-04-15 13:33:28 +0200

    fix result files for calculations

--- calculate_features.sh
+++ calculate_features.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-uv run evaluation/features/imt.py --report_file evaluation/features/all_images_2.json imgs/*
+uv run evaluation/features/imt.py --report_file evaluation/features/all_images.json imgs/*
--- calculate_iqa_metrics.sh
+++ calculate_iqa_metrics.sh
@@ -4,7 +4,7 @@ uv run pyiqa -t imgs/ arniqa brisque clipiqa+ hyperiqa ilniqe laion_aes liqe man
     | sed -n '/^{/,$p' \
     | sed '$!b; s/}[^}]*$/}/' \
     | python3 -c 'import sys, ast, json; print(json.dumps(ast.literal_eval(sys.stdin.read().replace("np.float64(", "(")), indent=4))' \
-    > evaluation/iqa/res_2.json
+    > evaluation/iqa/res.json
 
 
 
